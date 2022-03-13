e0a8ebd490cbe95f2edef55e175d6274025f6976
L-delara <92549675+L-delara@users.noreply.github.com>
2022-03-13 13:45:56 -0400

    rendered dictionary search results

---
+++ src/Meaning.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Meaning.css";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <span className="partOfSpeech">{props.meaning.partOfSpeech}</span>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
